dabc98611495bf1bb1087413d64f9d6079fb746a
ibizaman <ibizapeanut@gmail.com>
2023-08-09 20:35:00 -0700

    wip

--- modules/home-assistant.nix
+++ modules/home-assistant.nix
@@ -16,6 +16,13 @@ let
     export PATH=${pkgs.gnused}/bin:${pkgs.curl}/bin:${pkgs.jq}/bin
     exec ${pkgs.bash}/bin/bash ${ldap_auth_script_repo}/example_configs/lldap-ha-auth.sh $@
   '';
+
+  oidc_auth_script_raw = pkgs.writeText "oidc_auth.sh" (builtins.readFile ./home-assistant-oidc.sh);
+
+  oidc_auth_script = pkgs.writeShellScriptBin "oidc_auth.sh" ''
+    export PATH=${pkgs.gnused}/bin:${pkgs.curl}/bin:${pkgs.jq}/bin:${pkgs.coreutils}/bin
+    exec ${pkgs.bash}/bin/bash ${oidc_auth_script_raw} $@
+  '';
 in
 {
   options.shb.home-assistant = {
@@ -53,6 +60,12 @@ in
         backend = "restic";
         repositories = [];
       };
+    };
+
+    oidcEndpoint = lib.mkOption {
+      type = lib.types.str;
+      description = "OIDC endpoint for SSO";
+      example = "https://authelia.example.com";
     };
   };
 
@@ -102,10 +115,11 @@ in
             # Ensure you have the homeassistant provider enabled if you want to continue using your existing accounts
             { type = "homeassistant"; }
             { type = "command_line";
-              command = ldap_auth_script + "/bin/ldap_auth.sh";
+              command = oidc_auth_script + "/bin/oidc_auth.sh";
               # Only allow users in the 'homeassistant_user' group to login.
               # Change to ["https://lldap.example.com"] to allow all users
-              args = [ cfg.ldapEndpoint "homeassistant_user" ];
+              # args = [ cfg.ldapEndpoint "homeassistant_user" ];
+              args = [ cfg.oidcEndpoint ];
               meta = true;
             }
           ];
@@ -148,14 +162,135 @@ in
       http2 = true;
       sslCertificate = "/var/lib/acme/${cfg.domain}/cert.pem";
       sslCertificateKey = "/var/lib/acme/${cfg.domain}/key.pem";
+      # https://www.authelia.com/integration/proxies/nginx/
       extraConfig = ''
         proxy_buffering off;
       '';
       locations."/" = {
         proxyPass = "http://${toString config.services.home-assistant.config.http.server_host}:${toString config.services.home-assistant.config.http.server_port}/";
         proxyWebsockets = true;
+
+        extraConfig =
+          # From https://www.authelia.com/integration/proxies/nginx/#authelia-authrequestconf
+          ''
+          ## Send a subrequest to Authelia to verify if the user is authenticated and has permission to access the resource.
+          auth_request /authelia;
+
+          ## Set the $target_url variable based on the original request.
+
+          ## Comment this line if you're using nginx without the http_set_misc module.
+          # set_escape_uri $target_url $scheme://$http_host$request_uri;
+
+          ## Uncomment this line if you're using NGINX without the http_set_misc module.
+          set $target_url $scheme://$http_host$request_uri;
+ 
+          ## Save the upstream response headers from Authelia to variables.
+          auth_request_set $user $upstream_http_remote_user;
+          auth_request_set $groups $upstream_http_remote_groups;
+          auth_request_set $name $upstream_http_remote_name;
+          auth_request_set $email $upstream_http_remote_email;
+ 
+          ## Inject the response headers from the variables into the request made to the backend.
+          proxy_set_header Remote-User $user;
+          proxy_set_header Remote-Groups $groups;
+          proxy_set_header Remote-Name $name;
+          proxy_set_header Remote-Email $email;
+
+          ## If the subreqest returns 200 pass to the backend, if the subrequest returns 401 redirect to the portal.
+          error_page 401 =302 https://${cfg.oidcEndpoint}/?rd=$target_url;
+          ''
+          # From https://www.authelia.com/integration/proxies/nginx/#proxyconf
+          + ''
+          ## Headers
+          proxy_set_header Host $host;
+          proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Host $http_host;
+          proxy_set_header X-Forwarded-Uri $request_uri;
+          proxy_set_header X-Forwarded-Ssl on;
+          proxy_set_header X-Forwarded-For $remote_addr;
+          proxy_set_header X-Real-IP $remote_addr;
+          proxy_set_header Connection "";
+
+          ## Basic Proxy Configuration
+          client_body_buffer_size 128k;
+          proxy_next_upstream error timeout invalid_header http_500 http_502 http_503; ## Timeout if the real server is dead.
+          proxy_redirect  http://  $scheme://;
+          proxy_cache_bypass $cookie_session;
+          proxy_no_cache $cookie_session;
+          proxy_buffers 64 256k;
+
+          ## Trusted Proxies Configuration
+          ## Please read the following documentation before configuring this:
+          ##     https://www.authelia.com/integration/proxies/nginx/#trusted-proxies
+          # set_real_ip_from 10.0.0.0/8;
+          # set_real_ip_from 172.16.0.0/12;
+          # set_real_ip_from 192.168.0.0/16;
+          # set_real_ip_from fc00::/7;
+          # set_real_ip_from 127.0.0.1;
+          real_ip_header X-Forwarded-For;
+          real_ip_recursive on;
+
+          ## Advanced Proxy Configuration
+          send_timeout 5m;
+          proxy_read_timeout 360;
+          proxy_send_timeout 360;
+          proxy_connect_timeout 360;
+          '';
+      };
+
+      locations."/authelia" = {
+        proxyPass = "${cfg.oidcEndpoint}/api/verify";
+
+        # From https://www.authelia.com/integration/proxies/nginx/#authelia-locationconf
+        extraConfig = ''
+          internal;
+
+          proxy_set_header X-Original-URL $scheme://$http_host$request_uri;
+          proxy_set_header X-Original-Method $request_method;
+          proxy_set_header X-Forwarded-Method $request_method;
+          proxy_set_header X-Forwarded-Proto $scheme;
+          proxy_set_header X-Forwarded-Host $http_host;
+          proxy_set_header X-Forwarded-Uri $request_uri;
+          proxy_set_header X-Forwarded-For $remote_addr;
+          proxy_set_header Content-Length "";
+          proxy_set_header Connection "";
+
+          proxy_pass_request_body off;
+          proxy_next_upstream error timeout invalid_header http_500 http_502 http_503; # Timeout if the real server is dead
+          proxy_redirect http:// $scheme://;
+          proxy_http_version 1.1;
+          proxy_cache_bypass $cookie_session;
+          proxy_no_cache $cookie_session;
+          proxy_buffers 4 32k;
+          client_body_buffer_size 128k;
+
+          send_timeout 5m;
+          proxy_read_timeout 240;
+          proxy_send_timeout 240;
+          proxy_connect_timeout 240;
+        '';
       };
     };
+
+    # Rules from https://community.home-assistant.io/t/anyone-have-authelia-working-with-ha-to-handle-authentication/321233/18
+    shb.authelia.rules = [
+      {
+        domain = fqdn;
+        policy = "bypass";
+        resources = [
+          "^/api.*"
+          "^/auth/token.*"
+          "^/.external_auth=."
+          "^/service_worker.js"
+          "^/static/.*"
+        ];
+      }
+      {
+        domain = fqdn;
+        policy = "two_factor";
+      }
+    ];
 
     sops.secrets."home-assistant" = {
       inherit (cfg) sopsFile;
